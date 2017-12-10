5b0b41547aeb25cc2579df4ab9d3c077591d0887
Alex <dracken@gmail.com>
2017-12-10 00:15:03 -0800

    Keyboard support!

--- resources/script.js
+++ resources/script.js
@@ -1,10 +1,26 @@
 function updateNumbers(e){ 
 //This function will read the digits entered and store them until we are ready to add a complete number to our value
+    let digit = " ";
+    if(e.type === "click") {
+        digit = e.target.value;
+    }
+    else if(e.type="keydown")
+    {
+        digit = e.key;
+ 
+    }
+    
+    console.log(e);
+    if ((valueString.length > 0) &&(operationOrder.length === 0)) {valueString=[];history="";}
     if(digits.length<20){
-    digits.push(e.target.value);   
+        if ((digit === "." )&&(digits[digits.length-1]===".")) { 
+            updateDisplays(".","Please don't add more decimal points ");}
+        else{
+    digits.push(digit);   
     console.log("Pushed number into digits array.Print the digit array")
     console.log(digits);
     updateDisplays(digits.join(""));
+            }
     }
     else{
         updateDisplays("That's a lot of numbers!","This is a simple JS app, please hit clear and try something easier.");
@@ -15,12 +31,25 @@ function updateNumbers(e){
 
 function updateOperations(e){
 //This function will do some basic validation (no empty numbers, no duplicate operations) and update our valuestring with the full number as well as the operation that should take place
+    let operator ="";
+     if(e.type === "click") {
+        operator = e.target.value;
+    }
+    else if(e.type="keydown")
+    {
+        operator = e.key;
+        if(operator ==="Enter") {operator="=";}
+ 
+    }
     
-    const operator = e.target.value;
-    const composedNumber= digits.join("");
+    let composedNumber= digits.join("");
    
     if(composedNumber!=""){
-    valueString.push(composedNumber);
+        if(composedNumber === ".") {
+            composedNumber = "0.0";
+            }
+        
+        valueString.push(composedNumber);
         
      console.log("Digit array not blank. Pushed all numbers entered previously as one into value string. Print ValueString");
      console.log(valueString);
@@ -93,7 +122,10 @@ function clear(){
 function runTheMath(index){
 //This function handles the heavy lifting of the math as well as removing the operation from the list once it's been executed.
     
-    console.log("Running through the math function.");
+    console.log("Running through the math function. With the valueString & operationOrder");
+    console.log(valueString);
+    console.log(operationOrder);
+    
      if(operationOrder[index]==="+"){
     
         valueString.splice(index,2,(Number(valueString[index]) + Number(valueString[index+1])));
@@ -104,7 +136,12 @@ function runTheMath(index){
         operationOrder.splice(index,1);
     }
     else if(operationOrder[index]==="/"){
-         valueString.splice(index,2,(Number(valueString[index]) / Number(valueString[index+1])));
+        if (valueString[index+1] === "0") {
+            history="Division by 0 detected. Replaced with 1: ";
+            valueString.splice(index,2,(Number(valueString[index]) / 1));
+        }
+        else{ valueString.splice(index,2,(Number(valueString[index]) / Number(valueString[index+1])));}
+        
         operationOrder.splice(index,1);
     }
     
@@ -156,3 +193,28 @@ numbers.forEach(number=>number.addEventListener('click',updateNumbers));
 
 ac.addEventListener('click',clear);
 equal.addEventListener('click',operate);
+
+window.addEventListener('keydown', function(e){
+    const operations= /[/|+|-|*]/;
+    const equals =/[=|Enter]/g;
+    const numberz= /^[0-9]*$/;
+    const shift =/[Shift]/;
+    console.log(e);
+    console.log(equals.test(e.key));
+     console.log(!shift.test(e.key));            
+    console.log(((equals.test(e.key))&& !shift.test(e.key)));
+   
+   if((operations.test(e.key))&& !shift.test(e.key)){
+        console.log("Operations");
+        updateOperations(e);}
+    
+    else if((numberz.test(e.key))&& !shift.test(e.key)){
+     console.log("Number");
+        updateNumbers(e);}
+    
+    else if((equals.test(e.key)) ||(e.keyCode=13) && !shift.test(e.key)) {
+        console.log("equals");
+         updateOperations(e);
+         operate(e);}
+    else{ updateDisplays(e.key,"Please enter number");}
+                                             });
